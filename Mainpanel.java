042a2b66b7a6a4b7a79deae81ed2a0f3262e1719
raghulthangaraju <raghulthangaraju@gmail.com>
2017-05-02 12:00:11 +0530

    Add files via upload

---
+++ Mainpanel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm;
+
+/**
+ *
+ * @author windows 8
+ */
+
+class Mainpanel
+{
+	 public static void main(String[] args)
+        {
+               
+               Atm a= new Atm();
+               
+               
+        }
+        
+}
